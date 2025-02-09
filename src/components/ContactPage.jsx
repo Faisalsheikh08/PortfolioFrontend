82348d78955f26e65718a50cd1425ad1e75fc00e
Faisalsheikh08 <129953150+Faisalsheikh08@users.noreply.github.com>
2025-02-09 16:47:30 +0530

    Update ContactPage.jsx

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -56,7 +56,7 @@ export default function ContactPage() {
         </div>
 
         <div className="contact-form-container">
-          <h2>Send a message 🚀</h2>
+          <h2>Connect with me!</h2>
           <br />
           {status && <p className="message">{status}</p>}
           <form onSubmit={handleSubmit}>
